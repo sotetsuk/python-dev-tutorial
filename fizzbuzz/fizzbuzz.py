ba05cd82990b0b6dfb220056737c553bcccdca0c
Sotetsu KOYAMADA（小山田創哲） <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-08 02:09:39 +0900

    feat: implement fizzbuzz function (#6)

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,2 +1,9 @@
 def fizzbuzz(n: int) -> str:
-    return ""
+    if n % 15 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
+    else:
+        return str(n)
