286dea64f63354085920d9c3b0947c71f0753b8f
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-08 06:40:24 +0900

    feat: modify setup.py for installing cli

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fizzbuzz',
-    version="0.0.1",
+    version="0.0.2",
     description='Fizz Buzz program.',
     author='sotetsuk',
     url='https://github.com/sotetsuk/python-dev-tutorial',
@@ -10,6 +10,9 @@ setup(
     license='MIT',
     install_requires=[],
     packages=find_packages(exclude=["tests"]),
+    entry_points={
+        'console_scripts': 'fizzbuzz-cli = fizzbuzz.main:main'
+    },
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License"
