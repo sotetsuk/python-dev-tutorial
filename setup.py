e1ecedd397ba16b64638109c86008e8aff56d594
Sotetsu KOYAMADA <koyamada-s@sys.i.kyoto-u.ac.jp>
2020-10-08 07:43:02 +0900

    feat: modify setup.py for click dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='fizzbuzz',
-    version="0.0.2",
+    version="0.0.3",
     description='Fizz Buzz program.',
     author='sotetsuk',
     url='https://github.com/sotetsuk/python-dev-tutorial',
     author_email='koyamada-s@sys.i.kyoto-u.ac.jp',
     license='MIT',
-    install_requires=["pytest"],
+    install_requires=["pytest", "click"],
     packages=find_packages(exclude=["tests"]),
     entry_points={
         'console_scripts': 'fizzbuzz-cli = fizzbuzz.fizzbuzz:main'
